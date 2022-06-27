2ab251509e7266bc80cde7ec7f22d2e67b305f6c
Vinzent Bücheler <50985408+VinzentBuecheler@users.noreply.github.com>
2022-06-27 14:44:19 +0200

    Update geschichte.py

--- geschichte.py
+++ geschichte.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def app():
     st.title('Geschichte')
-    st.write('Willkommen zur Geschichte')
+    st.write('In diesem Bereich kannst du einer Geschichte folgen, bei welcher du mehrere Aufgaben durch Programmieren Lösen musst.')
     
